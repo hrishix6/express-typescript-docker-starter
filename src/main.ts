ee85c81d436991ae995f591d4593553f666fb06a
Hrishikesh Pethe <hrishikesh.pethe@tietoevry.com>
2022-12-12 10:42:33 +0530

    add http server bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-//Console.log("hello");
+import "dotenv/config";
+import http, {Server} from "http";
+import app from "./app";
+import {ConfigService} from "./config";
+
+ConfigService.load();
+
+function listenAsync(server:Server, port:number)
+{
+    return new Promise((resolve, reject)=> {
+        server.listen(port)
+            .once('listening',resolve)
+            .once('error',reject);
+    });
+}
+
+async function main()
+{
+    const server = http.createServer(app);
+
+    const port = process.env.PORT && parseInt(process.env.PORT);
+
+    await listenAsync(server,port || 5000);
+}
+
+main().catch(err => console.error);
